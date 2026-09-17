bd1f95a48d75ca7f7442f754dc51651b83d00a2b
MutedMute <hydrodarklord90@gmail.com>
2026-09-17 15:32:17 -0500

    Fixes

--- worlds/dst/Constants.py
+++ worlds/dst/Constants.py
@@ -378,12 +378,12 @@ DSTAP_ITEMS = [
     [428, "Void Repair Kit",             "voidcloth_kit",                             [ "nounlock", "useful" ],                    [ "MAGIC", "TOOLS" ] ],
     [429, "Brightshade Sword",           "sword_lunarplant",                          [ "nounlock", "useful" ],                    [ "MAGIC", "WEAPONS" ] ],
     [430, "Brightshade Armour",          "armor_lunarplant",                          [ "nounlock", "useful" ],                    [ "MAGIC", "ARMOUR" ] ],
-    [431, "Brightshade Helm",            "lunarplant_hat",                            [ "nounlock", "useful" ],                    [ "MAGIC", "ARMOUR" ] ],
+    [431, "Brightshade Helm",            "lunarplanthat",                             [ "nounlock", "useful" ],                    [ "MAGIC", "ARMOUR" ] ],
     [432, "Brightshade Staff",           "staff_lunarplant",                          [ "nounlock", "useful" ],                    [ "MAGIC", "WEAPONS" ] ],
     [433, "Brightshade Smasher",         "pickaxe_lunarplant",                        [ "nounlock", "useful" ],                    [ "MAGIC", "TOOLS" ] ],
     [434, "Brightshade Shoevel",         "shovel_lunarplant",                         [ "nounlock", "useful" ],                    [ "MAGIC", "TOOLS" ] ],
     [435, "Brightshade Repair Kit",      "lunarplant_kit",                            [ "nounlock", "useful" ],                    [ "MAGIC", "TOOLS" ] ],
-    [436, "Brightshade Bomb",            "bumb_lunarplant",                           [ "nounlock", "useful" ],                    [ "MAGIC", "WEAPONS" ] ],
+    [436, "Brightshade Bomb",            "bomb_lunarplant",                           [ "nounlock", "useful" ],                    [ "MAGIC", "WEAPONS" ] ],
 
     # Starting Recipes (451-500)
     [451, "Axe",                         "axe",                                   [ "basic", "progression" ] ],
@@ -606,7 +606,7 @@ DSTAP_LOCATIONS = [
     [275,   "Ancient Guardian",         "minotaur",                 [ "boss", "ruins", "priority_fuelweaver_boss" ] ],
     [276,   "Crab King",                "crabking",                 [ "boss", "raidboss", "ocean", "priority_celestial_boss", "special_hermit_10" ] ],
     [277,   "Crystal Deerclops",        "mutateddeerclops",         [ "boss", "seasonal", "winter", "seasons_passed_4" ] ],
-    [278,   "Armored Bearger",          "mutatedbearger",           [ "boss", "seasonal", "autumn", "seasons_passed_5" ] ],
+    [278,   "Armoured Bearger",          "mutatedbearger",           [ "boss", "seasonal", "autumn", "seasons_passed_5" ] ],
     [279,   "Possessed Varg",           "mutatedwarg",              [ "boss", "rng", "seasons_passed_4" ] ],
     [280,   "Celestial Revenent",       "alterguardian_phase1_lunarrift",          [ "boss", "raidboss", "ocean" ] ],
     [281,   "Enlightened W.A.R.B.O.T",  "wagboss_robot",            [ "boss", "raidboss", "ocean" ] ],
